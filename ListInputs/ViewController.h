d38d11dcf4c5b17d74078a3f260c162b3b8207c3
Vinay Raj <vinayraj.dunty@myntra.com>
2015-02-19 21:53:09 +0530

    Initial Commit

---
+++ ListInputs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ListInputs
+//
+//  Created by Vinay Raj Dunty on 19/02/15.
+//  Copyright (c) 2015 Myntra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ListInputs/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ListInputs
+//
+//  Created by Vinay Raj Dunty on 19/02/15.
+//  Copyright (c) 2015 Myntra. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
